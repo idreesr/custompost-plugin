d9d08cb169e8839e8356ef8372e5d2a50d4a516e
idreesr <idreesr@users.noreply.github.com>
2016-08-16 02:08:21 -0400

    made related posts shortcode
    also styled the shortcode

--- inc/custom_shortcode.php
+++ inc/custom_shortcode.php
@@ -1,43 +1,66 @@
 <?php
 
-// this function creates a form for users to input information in
-function form_shortcode($atts , $content=null) {
-	extract( shortcode_atts
-		(array(
-			'email'=>'me@me.com',
-			), $atts )
-		);
-	// returns a form that contains two small text boxes and one large text box
-	// also contains a submit and reset form button
-	return '<div class="form">
-		<form action="mailto:'.$email.'" method="post" name="theform" class="theform" enctype="text/plain">
-			<p>
-				<label for="fullname">Name:</label>
-				<br />
-				<input name="fullname" type="text" id="fullname" tabindex="1" placeholder="Enter your name here" size="50" />
-			</p>
-
-			<p>
-				<label for="email">Email Address:</label>
-				<br />
-				<input name="email" type="text" id="email" tabindex="6" placeholder="Enter your email" size="50" />
-			</p>
-
-			<p>
-				<label for="userinput">Your concern here:</label>
-				<br />
-				<textarea name="usertext" id="usertext" rows="8" col="60"></textarea>
-			</p>
-
-			<p>
-				<input name="submit" type="submit" value="Submit" tabindex="11" id="submit" />
-				<input name="reset" type="reset" value="Reset Form" tabindex="12" id="reset" />
-			</p>
-		</form>
-	</div>';
-
+/* this shortcode function creates a related posts section for posts */
+function rp_shortcode() {
+	ob_start();
+	// this code is based off of code from this: https://wordpress.org/support/topic/wp_query-based-on-the-current-post-categories
+	// declaring $post as global first
+	global $post;
+	// making new custom query that displays related posts (via category)
+	$this_post_category = wp_get_post_categories($post->ID);
+	// assigning the $current to the current post's ID
+	$current = $post->ID;
+	?>
+	<div class="related-posts">
+		<h2>Related Posts</h2>
+	<?php
+	// using post__not_in to ensure it doesn't display the current post
+	$new_query = new WP_Query(array( 'category__in'=> $this_post_category,
+	 'post__not_in' => array($current), 'posts_per_page' => 4) );
+	if ($new_query->have_posts()):
+		while ($new_query->have_posts()): $new_query->the_post(); ?>
+			<li class="rp-list">
+				<!-- this makes the captions link to the full
+				corresponding blog posts -->
+				<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
+					<p><?php the_title(); ?></p>
+				</a>
+			</li>
+		<?php endwhile; wp_reset_postdata(); // resets Post Data
+		endif; // ending the if-statement
+		?>
+	</div>
+	<?php
+	return ob_get_clean();
 }
 // this function adds a hook for a shortcode tag
-add_shortcode( 'form_shortcode', 'form_shortcode' );
+add_shortcode( 'rp_shortcode', 'rp_shortcode' );
+
+/* this shortcode function creates a signature */
+/* this is an extra shortcode that I made */
+function signature_shortcode($atts) {
+	// sets a default signature
+	// sets default signature font color, border color, and background color
+	extract( shortcode_atts
+		(array(
+			'statement'=>'Thanks for reading,',
+			'author'=>'Author',
+			'colour'=>'#9EAD68',
+			'background'=>'transparent',
+			), $atts )
+		);
+	return '<style type="text/css">
+
+			.signature {
+				background-color: '.$background.';
+				border-color: '.$colour.';
+				color: '.$colour.';
+			}
+
+			</style>
+	<div class="signature">' .$statement. '<br />' .$author. '</div>';
+}
+// this function adds a hook for a shortcode tag
+add_shortcode( 'signature_shortcode', 'signature_shortcode' );
 
 ?>
